1b4646f88ebc48337a7232449eb5cdb9ba240af6
Roman Ivaskevych <Roman.Ivaskevych@eleks.com>
2017-11-20 16:36:25 +0200

    #6 Create component for 'Custom' modal

--- src/index.js
+++ src/index.js
@@ -1,12 +1,57 @@
 import React from 'react'
+import {TextInput, View, StyleSheet, Dimensions} from 'react-native'
+import CustomModal from './components/CustomModal'
 import Icon from './components/Icon'
 
-const Test = () => {
-  return <Icon name='add'
-    reverse
-    color='#43A047'
-    onPress={() => console.log('open "Add activity modal"')}
-  />
+const window = Dimensions.get('window')
+class Test extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      modalVisible: false
+    }
+    this.setModalVisible = this.setModalVisible.bind(this)
+  }
+
+  setModalVisible (visible) {
+    this.setState({modalVisible: visible})
+  }
+
+  render () {
+    return (
+      <View style={styles.container}>
+        <Icon
+          containerStyle={styles.addButton}
+          name='add'
+          reverse
+          raised
+          color='#43A047'
+          onPress={() => this.setModalVisible(true)}
+        />
+        <CustomModal
+          modalVisible={this.state.modalVisible}
+          setModalVisible={this.setModalVisible}
+          onSubmit={() => true}
+          hideOnBackdropClick
+          title='Add Activity'
+        >
+          <TextInput />
+        </CustomModal>
+      </View>
+    )
+  }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column'
+  },
+  addButton: {
+    position: 'absolute',
+    right: 25,
+    top: window.height - 120
+  }
+})
 
 export default Test
